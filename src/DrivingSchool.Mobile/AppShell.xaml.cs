026c6f2ab523cdb41a7596ff7a579e56d683d815
Obj-Proger <obj.proger@gmail.com>
2026-06-18 03:36:15 +0500

    chore: initialize solution with 6 projects and dependencies

---
+++ src/DrivingSchool.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DrivingSchool.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/DrivingSchool.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DrivingSchool.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
